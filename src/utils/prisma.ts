5efa417c3f1f2ee90fdf5a67e7df797abc3968c7
Startmarshal <yurvovka@gmail.com>
2026-03-06 20:12:37 +0300

    prisma fix

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,18 +1,17 @@
-import {PrismaPg} from '@prisma/adapter-pg';
-import {PrismaClient} from '@/generated/prisma';
+import { PrismaClient } from '@/generated/prisma';
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL,
+  });
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-});
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
