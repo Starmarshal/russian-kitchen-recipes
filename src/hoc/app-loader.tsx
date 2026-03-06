aa8c5aaeef3c79d71f585e33fde5225d2591585e
Startmarshal <yurvovka@gmail.com>
2026-03-06 18:19:25 +0300

    added actions and store for recipe page

--- src/hoc/app-loader.tsx
+++ src/hoc/app-loader.tsx
@@ -4,6 +4,7 @@ import {useSession} from 'next-auth/react';
 import {useAuthStore} from '@/store/auth.store';
 import {useEffect} from 'react';
 import {useIngredientStore} from '@/store/ingredient.store';
+import {useRecipeStore} from '@/store/recipe.store';
 
 interface IProps {
   children: React.ReactNode;
@@ -13,6 +14,7 @@ const AppLoader = ({children}: IProps) => {
   const {data: session, status} = useSession();
   const {loadIngredients} = useIngredientStore();
   const {isAuth, setAuthState} = useAuthStore();
+  const {loadRecipes} = useRecipeStore();
 
   useEffect(() => {
     setAuthState(status, session);
@@ -23,6 +25,10 @@ const AppLoader = ({children}: IProps) => {
       loadIngredients();
     }
   }, [isAuth, loadIngredients]);
+
+  useEffect(() => {
+    loadRecipes();
+  }, [loadRecipes]);
 
   return <>{children}</>;
 };
